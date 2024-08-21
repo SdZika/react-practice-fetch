a12c9a2145afb245b017b5b8ce738f26b97aad6d
SdZika <markozivkovic1982@gmail.com>
2024-08-21 15:29:45 +0200

    new fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Meme } from "./components/Fatch";
 import { Joke } from "./components/Async";
+import { MyFetch } from "./components/Fetch";
 
 function App() {
   return (
     <div className="App">
       <Meme url="https://api.imgflip.com/get_memes"/>
       <Joke />
+      <MyFetch />
     </div>
   );
 }
---
+++ src/components/Fetch/index.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+export const MyFetch = () => {
+
+[imageUrl, setImageUrl] = useState<string | null>(null)
+
+fetch("https://api.giphy.com/v1/gifs/translate?api_key=uZjJbk2dG1bH0sJKMLY3ZU7rAn0mr2KU&s=cats", {mode: "cors"})
+    .then(response => response.json())
+    .then(response => setImageUrl(response.data.images.original.url))
+
+
+
+
+  return (
+    <div>
+    imageURL && (
+        <>
+          <h1>An image</h1>
+          <img src={imageURL} alt={"placeholder text"} />
+        </>)
+
+    </div>
+   
+  )
+}
